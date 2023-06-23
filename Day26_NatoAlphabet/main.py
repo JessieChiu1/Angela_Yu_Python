9f241c9811ac1565fb382db06b23184fa56f5e1b
JessieChiu1 <jessiechiu12@gmail.com>
2023-06-23 03:59:36 -0400

    Day 26 completed

--- Day26_NatoAlphabet/main.py
+++ Day26_NatoAlphabet/main.py
@@ -1,12 +1,31 @@
-numbers = [1,2,3]
-new_numbers = [n + 1 for n in numbers]
+from typing import List
 
-# print(new_numbers)
+import pandas as pd
 
-name = "Jessie"
-letters_list = [l for l in name]
-print(letters_list)
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
 
-new_list = [n * 2 for n in range(1,5)]
+df = pd.read_csv("nato_phonetic_alphabet.csv")
 
-print(new_list)
+# this works the same as the list comprehension
+# data_dict = df.set_index('letter')['code'].to_dict()
+
+# .items() for list and .iterrows() for csv
+# '_' used when a variable is not going to be used or reference in a loop. Acts as placeholder
+
+data_dict = {row.letter: row.code for _, row in df.iterrows()}
+
+
+# print(data_dict)
+# print(dataWithPandas)
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+# Prompt and convert to a char list
+word = input("Enter a word:").upper()
+letter_list = list(word)
+
+
+nato_list = [data_dict[i] for i in letter_list]
+
+print(nato_list)
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Angela_Yu_Python
 
 Go to subfolder and run `py main.py` to view project for each day
+
+## List Comprehension
+`new_list = [new_item for item in list if test]`
+
+## Dictionary Comprehension
+`new_dict = {new_key:new_value for (key, value) in dict.item() if test}`
