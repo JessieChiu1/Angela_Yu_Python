4d8a6bb91c3b194371cbd73d1ce39ad2ad52394a
JessieChiu1 <jessiechiu12@gmail.com>
2023-07-24 17:55:47 -0400

    checking if my info is secure

--- Day32_smtplib/main.py
+++ Day32_smtplib/main.py
@@ -1,3 +1,18 @@
 import smtplib
+import os
+from dotenv import load_dotenv
 
+# load variables from .env files into the environment
+load_dotenv()
+
+email_address = os.environ.get("EMAIL_ADDRESS")
+password = os.environ.get("PASSWORD")
+app_password = os.environ.get("APP_PASSWORD")
+to_email = os.environ.get("TO_EMAIL")
+
+# set up smtplib connection
 connection = smtplib.SMTP("smtp.gmail.com", port=587)
+
+# https://en.wikipedia.org/wiki/Transport_Layer_Security
+# this is necessary to make our connection secure
+connection.starttls()
