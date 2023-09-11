9cff00a89b4d0c55bf7cd773a0820bd3c81fa9b9
JessieChiu1 <jessiechiu12@gmail.com>
2023-09-11 18:17:47 -0400

    sheety put request working

--- Day39_40_FlightDealFinder/main.py
+++ Day39_40_FlightDealFinder/main.py
@@ -25,17 +25,32 @@ headers = {
 # fetch all the city from sheety into a list
 sheety_get_endpoint = "https://api.sheety.co/4552723523d54bde250ecd98364af758/flightDeals/prices"
 response = requests.get(url=sheety_get_endpoint, headers=headers)
-response_list = response.json()["prices"]
+city_data = response.json()["prices"]
 
 # list comprehension to get only the city name
-city_list = [row["city"] for row in response_list]
+city_list = [row["city"] for row in city_data]
 
 # =================================================
 # adding the IATA code for all the city with Sheety
 # =================================================
 
 # end with /[Object ID]
-sheety_edit_endpoint = "https://api.sheety.co/4552723523d54bde250ecd98364af758/flightDeals/prices/"
+sheety_edit_endpoint = "https://api.sheety.co/4552723523d54bde250ecd98364af758/flightDeals/prices"
 
 # Tequila endpoint
 IATA_query_endpoint = "https://api.tequila.kiwi.com/locations/query"
+
+for i in range(len(city_data)):
+    row_data = city_data[i]
+    print(row_data)
+    rowid = city_data[i]["id"]
+    edited_data = {
+        "price": {
+            "city": city_data[i]["city"],
+            "iataCode": "TESTING",
+            "lowestPrice": city_data[i]["lowestPrice"],
+            "id": rowid,
+        }
+    }
+    response = requests.put(url=f"{sheety_edit_endpoint}/{rowid}", json=edited_data, headers=headers)
+    print(response.text)
