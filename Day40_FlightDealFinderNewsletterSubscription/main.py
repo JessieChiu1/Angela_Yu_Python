b94ff9d2f7196c65cbc17da8536e1677e335f2af
JessieChiu1 <jessiechiu12@gmail.com>
2023-09-11 21:51:06 -0400

    Day 40 completed

--- Day40_FlightDealFinderNewsletterSubscription/main.py
+++ Day40_FlightDealFinderNewsletterSubscription/main.py
@@ -51,52 +51,54 @@ for i in range(len(city_data)):
         "sort": "price"
     }
 
-    response = requests.get(url=tequila_search_endpoint, headers=header, params=query)
-    response.raise_for_status()
-    search_result = response.json()
-
-    cheapest_flight = search_result["data"][0]
-
-    # get the lowest price of the current city
-    sheety_price = city_data[i]["lowestPrice"]
-
-    # update sheety price and send email
-    # ==================================
-    if cheapest_flight["price"] < sheety_price:
-        # update sheety price
-        # ===================
-        sheety_edit_endpoint = "https://api.sheety.co/4552723523d54bde250ecd98364af758/flightDeals/prices"
-
-        rowid = city_data[i]["id"]
-        edited_data = {
-            "price": {
-                "city": city_data[i]["city"],
-                "iataCode": city_data[i]["iataCode"],
-                "lowestPrice": cheapest_flight["price"],
-                "id": rowid,
-            }
-        }
-
-        response = requests.put(url=f"{sheety_edit_endpoint}/{rowid}", json=edited_data, headers=sheety_headers)
+    try:
+        response = requests.get(url=tequila_search_endpoint, headers=header, params=query)
         response.raise_for_status()
-        print(response.text)
+        search_result = response.json()
 
-        # sending an email alert
-        # ======================
+        cheapest_flight = search_result["data"][0]
 
-        # fetch all user in google sheet
-        user_sheety_get_endpoint = "https://api.sheety.co/4552723523d54bde250ecd98364af758/flightDeals/users"
-        response = requests.get(url=user_sheety_get_endpoint, headers=sheety_headers)
-        user_data = response.json()
+        # get the lowest price of the current city
+        sheety_price = city_data[i]["lowestPrice"]
 
-        # send email to everyone to alert price update
-        for user in user_data["users"]:
-            smtp_email = "smtp.gmail.com"
-            with smtplib.SMTP(smtp_email) as connection:
-                connection.starttls()
-                connection.login(user=email_address, password=email_password)
-                connection.sendmail(
-                    from_addr=email_address,
-                    to_addrs=user.email,
-                    msg=f"Subject: {search_result.flyFrom} price alert\n\nOnly ${search_result.price} to fly from {search_result.cityFrom}-{search_result.cityCodeFrom} at {search_result.flyFrom} airport to {search_result.cityTo}-{search_result.cityCodeTo} at {search_result.flyTo}, from {search_result.local_arrival} to {search_result.local_departure}",
-                )
+        # update sheety price and send email
+        # ==================================
+        if cheapest_flight["price"] < sheety_price:
+            # update sheety price
+            # ===================
+            sheety_edit_endpoint = "https://api.sheety.co/4552723523d54bde250ecd98364af758/flightDeals/prices"
+
+            rowid = city_data[i]["id"]
+            edited_data = {
+                "price": {
+                    "city": city_data[i]["city"],
+                    "iataCode": city_data[i]["iataCode"],
+                    "lowestPrice": cheapest_flight["price"],
+                    "id": rowid,
+                }
+            }
+
+            response = requests.put(url=f"{sheety_edit_endpoint}/{rowid}", json=edited_data, headers=sheety_headers)
+            response.raise_for_status()
+
+            # sending an email alert
+            # ======================
+
+            # fetch all user in google sheet
+            user_sheety_get_endpoint = "https://api.sheety.co/4552723523d54bde250ecd98364af758/flightDeals/users"
+            response = requests.get(url=user_sheety_get_endpoint, headers=sheety_headers)
+            user_data = response.json()
+
+            # send email to everyone to alert price update
+            for user in user_data["users"]:
+                smtp_email = "smtp.gmail.com"
+                with smtplib.SMTP(smtp_email) as connection:
+                    connection.starttls()
+                    connection.login(user=email_address, password=email_password)
+                    connection.sendmail(
+                        from_addr=email_address,
+                        to_addrs=user.email,
+                        msg=f"Subject: {search_result.flyFrom} price alert\n\nOnly ${search_result.price} to fly from {search_result.cityFrom}-{search_result.cityCodeFrom} at {search_result.flyFrom} airport to {search_result.cityTo}-{search_result.cityCodeTo} at {search_result.flyTo}, from {search_result.local_arrival} to {search_result.local_departure}",
+                    )
+    except IndexError:
+        print(f"there is no search result for {city_data[i].iataCode}")
