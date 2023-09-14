7e374537158ab74d456852cae32e7c1b1f406f39
JessieChiu1 <jessiechiu12@gmail.com>
2023-09-14 19:31:41 -0400

    find list of songs and artists name via web scrapping

--- Day46_MusicTimeMachine/main.py
+++ Day46_MusicTimeMachine/main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
 from spotipy.oauth2 import SpotifyOAuth
+import spotipy
 
 # ===========
 # load-dotenv
@@ -19,28 +20,44 @@ spotify_username = os.getenv("SPOTIFY_USERNAME")
 date = input("Which date do you want to create the spotify playlists from? Please enter date in YYYY-MM-DD format\n")
 url = f"https://www.billboard.com/charts/hot-100/{date}"
 
+# fetch html
 response = requests.get(url=url)
 html = response.text
-
 soup = BeautifulSoup(html, "html.parser")
 
-# =======
-# Spotipy
-# =======
+# isolate the song containers
+song_containers = soup.find_all(name="div", class_="o-chart-results-list-row-container")
+
+# find all the song names
+song_names = [song.find(name="h3", id="title-of-a-story").text.strip() for song in song_containers]
+# print(song_names)
+
+# find the artist name
+artist_names = [song.find_all(name="span")[1].text.strip() for song in song_containers]
+# print(artist_names[0])
+
+# ====================
+# Spotipy Access Token
+# ====================
 
 # https://spotipy.readthedocs.io/en/2.13.0/#spotipy.oauth2.SpotifyOAuth
 
 # this is like your 0Auth info
 # scope - what you allow the project can do
 # cache_path - saving the token somewhere
-sp_auth = SpotifyOAuth(
-    client_id=spotify_client_id,
-    client_secret=spotify_client_secret,
-    redirect_uri="http://example.com",
-    scope="playlist-modify-private",
-    cache_path="token.txt",
-    username=spotify_username,
-)
+# sp_auth = SpotifyOAuth(
+#     client_id=spotify_client_id,
+#     client_secret=spotify_client_secret,
+#     redirect_uri="http://example.com",
+#     scope="playlist-modify-private",
+#     cache_path="token.txt",
+#     username=spotify_username,
+# )
+#
+# # Send the OAuth object to get an access token
+# access_token_info = sp_auth.get_access_token()
 
-# Send the OAuth object to get an access token
-access_token_info = sp_auth.get_access_token()
+# ======================
+# Spotipy Querying Songs
+# ======================
+
