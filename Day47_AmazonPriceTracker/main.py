8077dfd7a03015c709145faf09456339008e488a
JessieChiu1 <jessiechiu12@gmail.com>
2023-09-16 18:28:34 -0400

    web scrapping amazon price improve

--- Day47_AmazonPriceTracker/main.py
+++ Day47_AmazonPriceTracker/main.py
@@ -2,8 +2,11 @@ import requests
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
+# ==================
+# Fetching html page
+# ==================
 
-url = "https://www.amazon.com/Hartz-Delectables-Squeeze-Interactive-Lickable/dp/B0BGT84G2R/ref=sr_1_1_sspa?crid=1T7CRSWCAAKPR&keywords=delectables%2Blickable%2Bcat%2Btreats&qid=1694741755&sprefix=delecta%2Caps%2C215&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1"
+# url = "https://www.amazon.com/Hartz-Delectables-Squeeze-Interactive-Lickable/dp/B0BGT84G2R/ref=sr_1_1_sspa?crid=1T7CRSWCAAKPR&keywords=delectables%2Blickable%2Bcat%2Btreats&qid=1694741755&sprefix=delecta%2Caps%2C215&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1"
 
 headers = {
     "Accept-Language": "en-US,en;q=0.9,zh-HK;q=0.8,zh;q=0.7",
@@ -20,9 +23,25 @@ soup = BeautifulSoup(html, "html.parser")
 # with open("index.html", "w", encoding="utf-8") as file:
 #     file.write(soup.prettify())
 
-product_info = soup.find(name="div", id="centerCol")
-div_price_container = product_info.find(name="div", id="corePrice_desktop")
+# ================================
+# Web scraping with beautiful soup
+# ================================
 
-price_container = div_price_container.find(name="span", class_="apexPriceToPay")
-price = float(price_container.text.split("$")[1])
-print(price)
+# the amazon html page will change depending on if the item is prime eligible or not
+
+# example of web scraping prime eligible item is in the "try" block
+# url = "https://www.amazon.com/Hartz-Delectables-Squeeze-Interactive-Lickable/dp/B0BGT84G2R/ref=sr_1_1_sspa?crid=1T7CRSWCAAKPR&keywords=delectables%2Blickable%2Bcat%2Btreats&qid=1694741755&sprefix=delecta%2Caps%2C215&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1"
+try:
+    product_info = soup.find(name="div", id="centerCol")
+    div_price_container = product_info.find(name="div", id="corePrice_desktop")
+
+    price_container = div_price_container.find(name="span", class_="apexPriceToPay")
+    price = float(price_container.text.split("$")[1])
+    print(price)
+# example of web scraping prime ineligible item
+# url = "https://www.amazon.com/dp/B0788884DF?ref=nb_sb_ss_w_as-reorder-t1_k4_1_11&amp=&crid=3GWM5HP0EPN08&sprefix=dermashield&th=1"
+except AttributeError:
+    price_whole = soup.find(name="span", class_="a-price-whole")
+    price_fraction = soup.find(name="span", class_="a-price-fraction")
+    price = float(f"{price_whole.text}{price_fraction.text}")
+    print(price)
