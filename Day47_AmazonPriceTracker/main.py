a58802d32da4d260a475b3a20877909f35aad20e
JessieChiu1 <jessiechiu12@gmail.com>
2023-09-17 23:21:00 -0400

    Day47 completed

--- Day47_AmazonPriceTracker/main.py
+++ Day47_AmazonPriceTracker/main.py
@@ -1,12 +1,22 @@
 import requests
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+import os
+import smtplib
+
+# =============
+# load_env
+# =============
+load_dotenv()
+
+email_address = os.environ.get("EMAIL_ADDRESS")
+password = os.environ.get("PASSWORD")
 
 # ==================
 # Fetching html page
 # ==================
 
-# url = "https://www.amazon.com/Hartz-Delectables-Squeeze-Interactive-Lickable/dp/B0BGT84G2R/ref=sr_1_1_sspa?crid=1T7CRSWCAAKPR&keywords=delectables%2Blickable%2Bcat%2Btreats&qid=1694741755&sprefix=delecta%2Caps%2C215&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1"
+url = "https://www.amazon.com/Hartz-Delectables-Squeeze-Interactive-Lickable/dp/B0BGT84G2R/ref=sr_1_1_sspa?crid=1T7CRSWCAAKPR&keywords=delectables%2Blickable%2Bcat%2Btreats&qid=1694741755&sprefix=delecta%2Caps%2C215&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1"
 
 headers = {
     "Accept-Language": "en-US,en;q=0.9,zh-HK;q=0.8,zh;q=0.7",
@@ -45,3 +55,24 @@ except AttributeError:
     price_fraction = soup.find(name="span", class_="a-price-fraction")
     price = float(f"{price_whole.text}{price_fraction.text}")
     print(price)
+
+
+product_name = soup.find(name="span", id="productTitle").text.strip()
+
+print(product_name)
+
+# ===================================================
+# Sending email when price drop below a certain price
+# ===================================================
+
+target_price = 35
+
+if price < target_price:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=email_address, password=password)
+        connection.sendmail(
+            from_addr=email_address,
+            to_addrs=email_address,
+            msg=f"Subject: Amazon Price Alert for \n\n {product_name} currently below ${target_price}"
+        )
