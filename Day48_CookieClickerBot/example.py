612ff96c92b8a4b1c5f1072cf320064e7407957b
JessieChiu1 <jessiechiu12@gmail.com>
2023-09-19 18:01:04 -0400

    practice problem completed

--- Day48_CookieClickerBot/example.py
+++ Day48_CookieClickerBot/example.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# Selenium is great because beautifulsoup fails when it takes time for the website to load or the website is built with React/Angular and it is responsive to user's click or interaction. With Selenium, you can mimic the same steps that the user might.
+
 # This is to add options to not automatically close chrome when program finish running
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -15,6 +17,30 @@ price_whole = driver.find_element(By.CLASS_NAME, value="a-price-whole")
 price_fraction = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
 price = float(f"{price_whole.text}.{price_fraction.text}")
 print(price)
+
+# ========================================
+# locate by CSS_SELECTOR are really useful
+# ========================================
+
+# url = "https://www.python.org/"
+#
+# document_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
+# link = document_link.text
+
+# ========================================
+# locate by XPath is best if all else fail
+# ========================================
+
+# Let's say you want to get the link for Submit Website Bug
+
+# url = "https://www.python.org/"
+
+# You can inspect element, right click, copy XPath
+# https://www.w3schools.com/xml/xpath_intro.asp
+
+# submit_bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+# print(submit_bug_link)
+
 
 # .quit will close out the whole driver, .close will close a tab
 driver.quit()
---
+++ Day48_CookieClickerBot/practice.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# ===========
+# init driver
+# ===========
+
+url = "https://www.python.org/"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+# =============
+# find elements
+# =============
+
+event_time = driver.find_elements(By.CSS_SELECTOR, value=".event-widget .menu time")
+event_link = driver.find_elements(By.CSS_SELECTOR, value=".event-widget .menu a")
+
+# =================
+# create dictionary
+# =================
+
+event_dict = []
+
+for i in range(0, len(event_time)):
+    event = {
+        "name": event_link[i].text,
+        "link": event_link[i].get_attribute("href"),
+        "date": event_time[i].text
+    }
+    event_dict.append(event)
+
+print(event_dict)
